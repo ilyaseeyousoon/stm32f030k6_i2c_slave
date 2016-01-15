583de77031f39ac6b1fb048ba9e045d7d6749b2e
toster <iljadlnv@gmail.com>
2016-01-16 02:05:26 +0300

    не проверял должно работать

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f030k6_i2c_slave' 
+ * Target:  'stm32f030k6_i2c_master Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/stm32f030k6_i2c_master Configuration/startup_stm32f030x6.d
@@ -0,0 +1,1 @@
+stm32f030k6_i2c_master configuration\startup_stm32f030x6.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm/startup_stm32f030x6.s
